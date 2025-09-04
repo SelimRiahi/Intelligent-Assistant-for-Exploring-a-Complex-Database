773535bbc3369926f296f18c8de352e61cfc7086
SelimRiahi <160012643+SelimRiahi@users.noreply.github.com>
2025-09-04 07:23:35 +0100

    Delete assistant-sql/db_config.py

--- assistant-sql/db_config.py
+++
@@ -1,7 +0,0 @@
-# db_config.py
-DB_CONFIG = {
-    'server': 'sql-assistant-1111.database.windows.net',
-    'database': 'db',
-    'username': 'sql-assistant-1111',
-    'password': 'Hamaselim123.'
-}
